aa102fe0e4795d9dcfbf714df7ba18d0788ba332
אלתר שרה < @C3>
2021-10-17 14:39:38 +0300

    Update (17/10/2021)

--- DAL/DataSource.cs
+++ DAL/DataSource.cs
@@ -26,8 +26,46 @@ namespace DalObject
         }
         static public void Initialize()
         {
+            Random rand = new Random();
+            for(int i = 0; i < 2; i++)
+            {
+                Station station = new Station[config.BaseStationsIndexer++];
+                station.ID = config.BaseStationsIndexer;
+                station.Name = config.BaseStationsIndexer;
+                station.Longitude = rand.Next();
+                station.Latitude = rand.Next();
+                station.ChargeSlots = rand.Next(10); //כמה רחפנים יש?
+            }
 
+            for(int i = 0; i < 5; i++)
+            {
+                Drone drone = new Drone[config.DronesIndexer++];
+                drone.ID = config.DronesIndexer;
+                drone.Model = config.DronesIndexer;
+                drone.MaxWeight = (WeightCategories)(rand.Next(0, 2));
+                drone.Status = (DroneStatus)(rand.Next(0,2));
+                drone.Battery = rand.Next(100);
+            }
 
+            for(int i = 0; i < 10; i++)
+            {
+                Customer customer = new Customer[config.customersIndexer++];
+                customer.ID = config.customersIndexer;
+                customer.Phone = $"{rand.Next(111111111, 999999999)}";
+                customer.Name = $"Customer{i}"
+                customer.longitude =  rand.Next();
+                customer.latitude =  rand.Next();
+            }
+
+            for(int i = 0; i < 10; i++)
+            {
+                Customer customer = new Customer[config.customersIndexer++];
+                customer.ID = config.customersIndexer;
+                customer.Phone = $"{rand.Next(111111111, 999999999)}";
+                customer.Name = $"Customer{i}"
+                customer.longitude =  rand.Next();
+                customer.latitude =  rand.Next();
+            }
         }
     }
 }
