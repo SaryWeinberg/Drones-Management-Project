8b91c9ec1fa67a11e605b45ed422de72674e8610
אלתר שרה < @C3>
2021-10-17 21:48:45 +0300

    Merge DalObject

--- DAL/dalObject.cs
+++ DAL/dalObject.cs
@@ -26,31 +26,63 @@ namespace DalObject
             DataSource.Stations[DataSource.config.StationsIndexer++] = station;
         }
 
-        public static void AddDrone()
+        public static void AddDrone(int id, string model, WeightCategories maxWeight, DroneStatus status ,double battery)
         {
+            Drone drone = new Drone();
+            drone.ID = id;
+            drone.Model = model;
+            drone.MaxWeight = maxWeight;
+            drone.Status = status;
+            drone.Battery = battery;
+            DataSource.Drones[DataSource.config.DronesIndexer++] = drone;
+
 
         }
-        public static void AddCustomer()
+        public static void AddCustomer(int id,string phone,string name,double Longitude,double Latitude)
         {
+            Customer customer = new Customer();
+            customer.ID = id;
+            customer.Phone = phone;
+            customer.Name = name;
+            customer.longitude = Longitude;
+            customer.latitude = Latitude;
+            DataSource.customers[DataSource.config.customersIndexer++] = customer;
 
         }
-        public static void AddParcel()
+        public static void AddParcel( int id,int senderId ,int targetId ,WeightCategories weight,Priorities priority,DateTime requested,int droneId ,DateTime scheduled,DateTime pickedUp ,DateTime delivered )
         {
-
+            Parcel parcel = new Parcel();
+            parcel.ID = id;
+            parcel.SenderId = senderId;
+            parcel.TargetId = targetId;
+            parcel.Weight = weight;
+            parcel.Priority = priority;
+            parcel.Requested = requested;
+            parcel.DroneId = droneId;
+            parcel.Scheduled = scheduled;
+            parcel.PickedUp = pickedUp;
+            parcel.Delivered = delivered;
+            DataSource.Parcels[DataSource.config.ParcelsIndexer++] = parcel;
         }
+        //public static void Update()
+        //{
+
+
+        //}
 
         //Updats//
 
         public static void AssingParcelToDrone(Parcel parcel)
         {
-            foreach(Drone drone in DataSource.Drones)
+            foreach (Drone drone in DataSource.Drones)
             {
-                if(drone.Status == DroneStatus.Available && drone.MaxWeight >= parcel.Weight)
+                if (drone.Status == DroneStatus.Available && drone.MaxWeight >= parcel.Weight)
                 {
                     parcel.DroneId = drone.ID;
                     return;
                 }
             }
+
         }
 
         public static void CollectParcelByDrone()
