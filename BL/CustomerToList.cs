3d4714421d3047b64d69205220ca6ea003c91a2e
SaryAlter <handesaim3a2@mbjcomp.org.il>
2021-11-11 12:53:51 +0200

    Add items to list

---
+++ BL/CustomerToList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBL.BO
+{
+    class CustomerToList
+    {
+        public ulong ID { get; set; }
+        public string Name { get; set; }
+        public ulong PhoneNum { get; set; }
+        public Location location { get; set; }
+
+        public List<DeliveryToCustomer> DeliveryToCustomer = new List<DeliveryToCustomer>();
+
+        public List<DeliveryToCustomer> DeliveryFromCustomer = new List<DeliveryToCustomer>();
+    }
+}
---
+++ BL/StationToList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBL.BO
+{
+    class StationToList
+    {
+        public int ID { get; set; }
+        public int Name { get; set; }
+        public Location location { get; set; }
+        public double AveChargeSlots { get; set; }
+
+        public List<DroneInCharge> DronesInCharge = new List<DroneInCharge>();
+    }
+}
