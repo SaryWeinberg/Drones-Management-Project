3599785123a4108f050396b0d0c058e3a12e6700
SaryAlter <handesaim3a2@mbjcomp.org.il>
2021-12-02 20:23:05 +0200

    add windows ctor

--- PL/DroneWindow.xaml.cs
+++ PL/DroneWindow.xaml.cs
@@ -25,14 +25,43 @@ namespace PL
         {
             InitializeComponent();
             bl = blMain;
-            DroneStatus.ItemsSource = Enum.GetValues(typeof(DroneStatus));
+            string[] dataArr = { "ID", "max_weight" , "model" , "station_ID" };
+
+            int x = 70;
+            foreach (string item in dataArr)
+            {
+                TextBlock droneItemT = new TextBlock();
+                droneItemT.Text = $"Enter parcel {item}:";
+                droneItemT.TextWrapping = TextWrapping.Wrap;
+                droneItemT.VerticalAlignment = VerticalAlignment.Top;
+                droneItemT.Margin = new Thickness(43, x, 0, 0);
+                DroneData.Children.Add(droneItemT);
+                if (item == "weight")
+                {
+                    ComboBox droneItemC = new ComboBox();
+                    droneItemC.Name = $"drone{item}";
+                    droneItemC.VerticalAlignment = VerticalAlignment.Top;
+                    droneItemC.Margin = new Thickness(199, x, 0, 0);
+                    droneItemC.ItemsSource = Enum.GetValues(typeof(WeightCategories));
+                    DroneData.Children.Add(droneItemC);
+                }
+                else
+                {
+                    TextBox droneItem = new TextBox();
+                    droneItem.Name = $"drone{item}";
+                    droneItem.TextWrapping = TextWrapping.Wrap;
+                    droneItem.VerticalAlignment = VerticalAlignment.Top;
+                    droneItem.Margin = new Thickness(199, x, 0, 0);
+                    DroneData.Children.Add(droneItem);
+                }
+                x += 30;
+            }
         }
 
         public DroneWindow(IBL.IBL blMain, IBL.BO.DroneBL drone)
         {
             InitializeComponent();
             bl = blMain;
-            DroneStatus.ItemsSource = Enum.GetValues(typeof(DroneStatus));
         }
-    }
+    }    
 }
