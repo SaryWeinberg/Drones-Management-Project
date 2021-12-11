6126fb3c675396a3fa9cfcc2baf65fee00dc7bf5
GittyBeckerman <handesaim3a3@mbjcomp.org.il>
2021-12-11 19:34:30 +0200

    Some changes...

--- PL/DroneWindow.xaml.cs
+++ PL/DroneWindow.xaml.cs
@@ -98,21 +98,59 @@ namespace PL
             */
 
 
-            /*            TextBlock Location = new TextBlock();
-                        Location.Text = "drone location";
-                        droneWin.Children.Add( Location);
-                        Location.TextWrapping = TextWrapping.Wrap;
-                        Location.VerticalAlignment = VerticalAlignment.Top;
-                        Location.Margin = new Thickness(43, 162,0,0);
+            TextBlock Location = new TextBlock();
+            Location.Text = "drone location";
+            DroneData.Children.Add(Location);
+            Location.TextWrapping = TextWrapping.Wrap;
+            Location.VerticalAlignment = VerticalAlignment.Top;
+            Location.Margin = new Thickness(43, 162, 0, 0);
 
+            TextBox LocationData = new TextBox();
+            LocationData.Text = (drone.Location).ToString();
+            DroneData.Children.Add(LocationData);
+            LocationData.TextWrapping = TextWrapping.Wrap;
+            LocationData.VerticalAlignment = VerticalAlignment.Top;
+            LocationData.Margin = new Thickness(199, 162, 0, 0);
+            Location.IsEnabled = false;
+
+            TextBlock BatteryStatus = new TextBlock();
+            BatteryStatus.Text = $"Battery Status {drone.BatteryStatus}";
+            DroneData.Children.Add(BatteryStatus);
+            BatteryStatus.TextWrapping = TextWrapping.Wrap;
+            BatteryStatus.VerticalAlignment = VerticalAlignment.Top;
+            BatteryStatus.Margin = new Thickness(43, 192, 0, 0);
+
+/*            TextBox LocationData = new TextBox();
+            LocationData.Text = (drone.Location).ToString();
+            DroneData.Children.Add(LocationData);
+            LocationData.TextWrapping = TextWrapping.Wrap;
+            LocationData.VerticalAlignment = VerticalAlignment.Top;
+            LocationData.Margin = new Thickness(199, 162, 0, 0);
+            Location.IsEnabled = false;*/
+
+            TextBlock Status = new TextBlock();
+            Status.Text = $"Drone Status {drone.Status}";
+            DroneData.Children.Add(Status);
+            Status.TextWrapping = TextWrapping.Wrap;
+            Status.VerticalAlignment = VerticalAlignment.Top;
+            Status.Margin = new Thickness(43, 222, 0, 0);
+            /*
                         TextBox LocationData = new TextBox();
                         LocationData.Text = (drone.Location).ToString();
-                        droneWin.Children.Add(LocationData);
+                        DroneData.Children.Add(LocationData);
                         LocationData.TextWrapping = TextWrapping.Wrap;
                         LocationData.VerticalAlignment = VerticalAlignment.Top;
                         LocationData.Margin = new Thickness(199, 162, 0, 0);
-                        Location.IsEnabled = false;
-            */
+                        Location.IsEnabled = false;*/
+
+            TextBlock Parcel = new TextBlock();
+            Parcel.Text = "parcel ID" + (drone.Parcel);
+            DroneData.Children.Add(Parcel);
+            Parcel.TextWrapping = TextWrapping.Wrap;
+            Parcel.VerticalAlignment = VerticalAlignment.Top;
+            Parcel.Margin = new Thickness(43, 252, 0, 0);
+            // ID + " model:  " + Model + " MaxWeight: " + MaxWeight + " BatteryStatus: " + BatteryStatus + " Status: " + Status + " Parcel: " + Parcel + " Location: " + Location;
         }
+        
     }
 }
