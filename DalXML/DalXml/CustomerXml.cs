7867be51a4b703094b82b5c3e8a2dea69d08babd
SaryAlter <handesaim3a2@mbjcomp.org.il>
2022-01-13 11:49:18 +0200

    change...

---
+++ DalXML/DalXml/CustomerXml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DalApi;
+using DO;
+
+namespace Dal
+{
+    public partial class DalXml : IDal
+    {
+        /// <summary>
+        /// Adding new customer to Database
+        /// </summary>
+        /// <param name="customer"></param>
+        public void AddCustomer(Customer customer)
+        {
+            IEnumerable<Customer> customerList = GetCustomers();
+            customerList.ToList().Add(customer);
+            XmlTools.SaveListToXmlSerializer(customerList, dir + customerFilePath);
+        }
+
+        /// <summary>
+        /// Update customer in DataBase
+        /// </summary>
+        /// <param name="customer"></param>
+        public void UpdateCustomer(Customer customer)
+        {
+            List<Customer> customerList = GetCustomers().ToList();
+            customerList[customerList.FindIndex(c => c.ID == customer.ID)] = customer;
+            XmlTools.SaveListToXmlSerializer(customerList, dir + customerFilePath);
+        }
+
+        /// <summary>
+        /// Returns a specific customer by ID number
+        /// </summary>
+        /// <param name="customerId"></param>
+        /// <returns></returns>
+        public Customer GetSpesificCustomer(int customerId)
+        {
+            try { return GetCustomers(c => c.ID == customerId).First(); }
+            catch (Exception) { throw new ObjectDoesNotExist("Customer", customerId); }
+        }
+
+        /// <summary>
+        /// Returns the customer list
+        /// </summary>
+        /// <param name="condition"></param>
+        /// <returns></returns>
+        public IEnumerable<Customer> GetCustomers(Predicate<Customer> condition = null)
+        {
+            condition ??= (c => true);
+            return from customer in XmlTools.LoadListFromXmlSerializer<Customer>(dir + customerFilePath)
+                   where condition(customer)
+                   select customer;
+        }     
+    }
+}
---
+++ DalXML/DalXml/StationXml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DalApi;
+using DO;
+
+namespace Dal
+{
+    public partial class DalXml : IDal
+    {
+        /// <summary>
+        /// Adding new station to DataBase
+        /// </summary>
+        /// <param name="station"></param>
+        public void AddStation(Station station)
+        {
+            IEnumerable<Station> stationList = GetStations();
+            stationList.ToList().Add(station);
+            XmlTools.SaveListToXmlSerializer(stationList, dir + stationFilePath);
+        }
+
+        /// <summary>
+        /// Update station in DataBase
+        /// </summary>
+        /// <param name="station"></param>
+        public void UpdateStation(Station station)
+        {
+            List<Station> stationList = GetStations().ToList();
+            stationList[stationList.FindIndex(s => s.ID == station.ID)] = station;
+            XmlTools.SaveListToXmlSerializer(stationList, dir + stationFilePath);
+        }
+
+        /// <summary>
+        /// Returns a specific station by ID number
+        /// </summary>
+        /// <param name="stationId"></param>
+        /// <returns></returns>
+        public Station GetSpesificStation(int stationId)
+        {
+            try { return GetStations(s => s.ID == stationId).First(); }
+            catch (Exception) { throw new ObjectDoesNotExist("Station", stationId); }
+        }
+
+        /// <summary>
+        /// Returns the station list
+        /// </summary>
+        /// <param name="condition"></param>
+        /// <returns></returns>
+        public IEnumerable<Station> GetStations(Predicate<Station> condition = null)
+        {
+            condition ??= (s => true);
+            return from station in XmlTools.LoadListFromXmlSerializer<Station>(dir + stationFilePath)
+                   where condition(station)
+                   select station;
+        }        
+    }
+}
